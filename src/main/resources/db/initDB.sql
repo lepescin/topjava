e31178861f45288cc5ad69ef47c706a98620c416
lepescin <74877244+lepescin@users.noreply.github.com>
2021-02-23 16:52:53 +0300

    HW3 without optional

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -23,3 +23,14 @@ CREATE TABLE user_roles
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+    id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    user_id          INTEGER                           NOT NULL,
+    calories         INTEGER                           NOT NULL,
+    description      VARCHAR                           NOT NULL,
+    dateTime         TIMESTAMP                         NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+CREATE UNIQUE INDEX user_meal_timestamp_idx ON meals (user_id, dateTime);
